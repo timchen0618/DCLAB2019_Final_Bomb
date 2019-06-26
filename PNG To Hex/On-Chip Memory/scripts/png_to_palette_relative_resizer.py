b397e3bd3cac5359e8fa19e2d05990d3f31e6bfc
jackchen03 <46782913+jackchen03@users.noreply.github.com>
2019-06-27 00:18:21 +0800

    Add files via upload

---
+++ PNG To Hex/On-Chip Memory/scripts/png_to_palette_relative_resizer.py
@@ -0,0 +1,42 @@
+from PIL import Image
+from collections import Counter
+from scipy.spatial import KDTree
+import numpy as np
+def hex_to_rgb(num):
+    h = str(num)
+    return int(h[0:4], 16), int(('0x' + h[4:6]), 16), int(('0x' + h[6:8]), 16)
+def rgb_to_hex(num):
+    h = str(num)
+    return int(h[0:4], 16), int(('0x' + h[4:6]), 16), int(('0x' + h[6:8]), 16)
+filename = input("What's the image name? ")
+new_w, new_h = map(int, input("What's the new height x width? Like 28 28. ").split(' '))
+palette_hex = ['0xa915f0', '0xaaea66', '0x3CABDD', '0x93CF81', '0x98E7D8', '0x37E2D5', '0x22BBE6', '0xFCC656', '0xA87C07', '0xCB8C1A', '0xE78924', '0xF2AB45', '0xA0611F', '0x0964c8', '0xfbfef2', '0x39403a']
+palette_rgb = [hex_to_rgb(color) for color in palette_hex]
+
+pixel_tree = KDTree(palette_rgb)
+im = Image.open("../sprite_originals/" + filename+ ".png") #Can be many different formats.
+im = im.convert("RGBA")
+im = im.resize((new_w, new_h),Image.ANTIALIAS) # regular resize
+pix = im.load()
+pix_freqs = Counter([pix[x, y] for x in range(im.size[0]) for y in range(im.size[1])])
+pix_freqs_sorted = sorted(pix_freqs.items(), key=lambda x: x[1])
+pix_freqs_sorted.reverse()
+print(pix)
+outImg = Image.new('RGB', im.size, color='white')
+outFile = open("../sprite_bytes/" + filename + '.txt', 'w')
+i = 0
+for y in range(im.size[1]):
+    for x in range(im.size[0]):
+        pixel = im.getpixel((x,y))
+        print(pixel)
+        if(pixel[3] < 200):
+            outImg.putpixel((x,y), palette_rgb[0])
+            outFile.write("%x\n" % (0))
+            print(i)
+        else:
+            index = pixel_tree.query(pixel[:3])[1]
+            outImg.putpixel((x,y), palette_rgb[index])
+            outFile.write("%x\n" % (index))
+        i += 1
+outFile.close()
+outImg.save("../sprite_converted/" + filename + ".png")
