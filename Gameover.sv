d98a2afeac8097c4f59511f0306cb960262d0ff0
Daniel <46804281+danielpoochai@users.noreply.github.com>
2019-06-26 20:12:34 +0800

    Add files via upload

--- Gameover.sv
+++ Gameover.sv
@@ -6,45 +6,90 @@ module Gameover(
 	input [7:0] p1_cor,
 	input [7:0] p2_cor,
 	//output
-	output logic gameover,
-	output logic [1:0] winner
+	output logic [2:0] gameover_state
 	);
 		
-	parameter NONE = 2'd0;
-	parameter P1 = 2'd1;
-	parameter P2 = 2'd2;
-	parameter TIE = 2'd3;
+	
+	parameter NONE = 3'd0;
+	parameter GAMEOVER = 3'd1;
+	parameter P1 = 3'd2;
+	parameter P2 = 3'd3;
+	parameter FINISH = 3'd4;
 
-	logic gameover_nxt;
-	logic [1:0] winner_nxt;
+	parameter P1_WIN = 1'd0;
+	parameter P2_WIN = 1'd1;
+
+	logic [6:0] gameover_ctr, gameover_ctr_nxt;
+	logic [2:0] gameover_state_nxt;
+	logic winner, winner_nxt;
 
 	always_ff @(posedge clk or posedge reset) begin 
 		if(reset) begin
-			 gameover <= 1'd0;
-			 winner <= 2'd0;
+			winner <= 1'd0;
+			gameover_state <= 3'd0;
+			gameover_ctr <= 7'd0;
 		end else begin
-			 gameover <= gameover_nxt;
-			 winner <= winner_nxt;
+			winner <= winner_nxt;
+			gameover_state <= gameover_state_nxt;
+			gameover_ctr <= gameover_ctr_nxt;
 		end
 	end
 
 	always_comb begin
-		if(i_explode[p1_cor] == 1'd1 && i_explode[p2_cor] == 1'd1) begin //die at the same time
-			gameover_nxt = 1'd1;
-			winner_nxt = TIE;
-		end
-		else if(i_explode[p1_cor] == 1'd1) begin //P1 die
-			gameover_nxt = 1'd1;
-			winner_nxt = P2;
-		end
-		else if(i_explode[p2_cor] == 1'd1) begin //P2 die
-			gameover_nxt = 1'd1;
-			winner_nxt = P1;
-		end
-		else begin
-			gameover_nxt = 1'd0;
-			winner_nxt = NONE;
-		end
+		gameover_state_nxt = gameover_state;
+		winner_nxt = winner;
+		gameover_ctr_nxt = gameover_ctr;
+
+		case(gameover_state)
+			NONE:
+			begin
+				if (i_explode[p1_cor] == 1'd1) begin
+					gameover_state_nxt = GAMEOVER;
+					winner_nxt = P2_WIN;
+				end
+				if (i_explode[p2_cor] == 1'd1) begin
+					gameover_state_nxt = GAMEOVER;
+					winner_nxt = P1_WIN;
+				end
+			end
+			GAMEOVER:
+			begin
+				if(gameover_ctr >= 7'd60) begin
+					if(winner == P1_WIN) begin
+						gameover_state_nxt = P1;
+					end
+					else begin
+						gameover_state_nxt = P2;
+					end
+					gameover_ctr_nxt = 0;
+				end
+				else gameover_ctr_nxt = gameover_ctr + 7'd1;
+			end
+			P1:
+			begin
+				if(gameover_ctr >= 7'd90) begin
+					gameover_state_nxt = FINISH;
+				end
+				else gameover_ctr_nxt = gameover_ctr + 7'd1;
+			end
+			P2:
+			begin
+				if(gameover_ctr >= 7'd90) begin
+					gameover_state_nxt = FINISH;
+				end
+				else gameover_ctr_nxt = gameover_ctr + 7'd1;
+			end
+			FINISH:
+			begin
+				gameover_state_nxt = gameover_state;
+			end
+			default:
+			begin
+				gameover_state_nxt = gameover_state;
+				winner_nxt = winner;
+				gameover_ctr_nxt = gameover_ctr;
+			end
+		endcase
 	end
 
 endmodule // Gameover
